7b7aa2f3631a7927458f55885251c77fdf54d834
t-likant <t-likant@hotmail.com>
2019-12-12 13:16:47 -0500

    Fc

---
+++ Assets/Mc.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Mc
+: MonoBehaviour
+{
+    public float speed = 0.9f; // Determina la velocidad con la que se mueve el objeto
+    float mouseX; // Declaracion de mouse X
+    void Start()
+
+    {
+    }
+
+
+
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition; // Analiza la posicion del cursor
+        mouseX += Input.GetAxis("Mouse X"); // Actualiza los valores de mouseX
+        transform.eulerAngles = new Vector3(0, mouseX, 0); // Actualiza la rotacion horizontal
+
+
+        if (Input.GetKey(KeyCode.W)) // Condicion para moverse hacia adelante con la tecla W
+        {
+            transform.position += transform.forward * speed; // Transforma la posicion hacia el frente
+        }
+        if (Input.GetKey(KeyCode.S)) // Condicion para moverse hacia atras con la tecla S
+        {
+            transform.position -= transform.forward * speed; // Transforma la posicion hacia atras
+        }
+        if (Input.GetKey(KeyCode.A)) // Condicion para moverse hacia la izquierda con la tecla A
+        {
+            transform.position -= transform.right * speed; // Transforma la posicion hacia la izquierda
+        }
+        if (Input.GetKey(KeyCode.D)) // Condicion para moverse hacia la derecha con la tecla D
+        {
+            transform.position += transform.right * speed; // Transforma la posicion hacia la derecha
+        }
+    }
+}
---
+++ Assets/Pc.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Pc : MonoBehaviour // Script/Clase asociado a la camara
+{
+    float mouseX; // Declaracion de mouse X
+    float mouseY; // declaracion de mouse Y
+    public bool InvertedMouse; // Condiciona la camara invertida
+
+    void Start()
+    {
+        transform.eulerAngles = new Vector3(30, 0, 0); // Establece la rotacion de la camara en los angulos 30 0 0
+    } 
+
+    void Update()
+
+    {
+        Vector3 mousePosition = Input.mousePosition; // Nos permite analizar la posicion del mouse en pantalla
+        mouseX += Input.GetAxis("Mouse X"); // La actualizacion es mas agradable y sin temblor
+
+        if (mouseY <= 40 && mouseY >= -40) // Condicional para que el cuerpo no se de la vuelta y ponga el mundo de cabeza
+        {
+            if (InvertedMouse) // Si InvertedMouse es verdadero se invierte la camara en Y
+            {
+                mouseY += Input.GetAxis("Mouse Y"); // Arriba es Abajo y viceversa
+            }
+            else
+            {
+                mouseY -= Input.GetAxis("Mouse Y"); // Arriba es arriba y abajo es abajo en la camara
+            }
+        }
+        else if (mouseY > 40) // Establece a mouseY en el limite positivo
+        {
+            mouseY = 40; // Limite positivo
+        }
+        else // Establece a mouseY en el limite negativo
+        {
+            mouseY = -40; // Limite negativo
+        }
+        Debug.Log(mouseX); // Nos muestra la rotacion en X
+        transform.eulerAngles = new Vector3(mouseY, mouseX, 0); // Rotacion en X y Y
+    }
+}
